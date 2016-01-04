02715dd97f74b198c904c3e5b5439fe54e10fa56
PhilipTrauner <philip.trauner@aol.com>
2016-01-04 03:57:00 +0100

    Moved Routing from Server/Server.py to own file

---
+++ Shared/Routing.py
@@ -0,0 +1,17 @@
+class Route:
+	def __init__(self, **kwargs):
+		self.setup(**kwargs)
+
+	def setup(self, **kwargs):
+		pass
+
+	def run(self, data, handler):
+		pass
+
+
+def create_routes(routes):
+	routes = routes.copy()
+	for prefix in routes:
+		if type(routes[prefix]) is tuple or type(routes[prefix]) is list:
+			routes[prefix] = routes[prefix][0](**routes[prefix][1])
+	return routes
