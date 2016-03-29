444f169b86ee578eb8fd02c907a07f09ab612412
PhilipTrauner <philip.trauner@aol.com>
2016-03-29 23:35:58 +0200

    Added function collection

--- Shared/Utils.py
+++ Shared/Utils.py
@@ -4,3 +4,11 @@ from sys import exc_info
 def capture_trace():
 	exc_type, exc_value, exc_traceback = exc_info()
 	print_exception(exc_type, exc_value, exc_traceback)
+
+def is_socket_related_error(error):
+	if type(error) not in (BrokenPipeError, ConnectionResetError, OSError):
+		return False
+	if type(error) is OSError:
+		if str(error) not in ("Connection closed", "[Errno 9] Bad file descriptor"):
+			return False
+	return True
